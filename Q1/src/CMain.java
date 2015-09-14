b056833c807369f10b676db60634f224349135dd
w1zgam3r <jflaquer@udel.edu>
2015-09-13 23:15:38 -0400

    Finished QB rating calculator

--- Q1/src/CMain.java
+++ Q1/src/CMain.java
@@ -1,8 +1,54 @@
+import java.util.Scanner;
 
 public class CMain {
 
 	public static void main(String[] args) {
-		// Test
+		// Declaring Variables
+		double td;
+		double yards;
+		double inter;
+		double comp;
+		double att;
+		
+		//Scanner to ask for input
+		Scanner user_input = new Scanner(System.in);
+		
+		System.out.print("How many touchdowns does the player have: ");
+		td = user_input.nextDouble();
+		System.out.print("How many total yards: ");
+		yards = user_input.nextDouble();
+		System.out.print("How many interceptions: ");
+		inter = user_input.nextDouble();
+		System.out.print("How many completions: ");
+		comp = user_input.nextDouble();
+		System.out.print("How many attempted passes: ");
+		att = user_input.nextDouble();
+		
+		//Declaring more variables
+		double cp;
+		double ypa;
+		double tpa;
+		double ipa;
+		double pr;
+		
+		//Completions per attempt
+		cp = (comp/att - .3)*5;
+		
+		//Yards per attempt
+		ypa = (yards/att -3)*.25;
+		
+		//Touchdowns per attempt
+		tpa = (td/att)*20;
+				
+		//Interceptions per attempt
+		ipa = 2.375 - ((inter/att)*25);
+		
+		//Passer rating
+		pr = ((cp+ypa+tpa+ipa)/6)*100;
+		
+		//Final output
+		System.out.print("The player's QB rating is: "+pr);
+		
 
 	}
 
